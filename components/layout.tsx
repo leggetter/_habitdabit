15aa7146b5e9190584ac20b7d2e3929407793943
Phil Leggetter <phil@leggetter.co.uk>
2023-04-05 11:55:03 +0100

    chore: add <title> to <Layout />

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,16 +2,26 @@ import Header from "./header";
 import Footer from "./footer";
 import type { ReactNode } from "react";
 import { Box } from "@chakra-ui/react";
+import Head from "next/head";
 
 export default function Layout({
   children,
+  title,
   errors,
 }: {
   children: ReactNode;
+  title?: string;
   errors?: string[];
 }) {
+  let displayTitle = title
+    ? `${title} - HabitDabit`
+    : "HabitDabit - create positive habits through repetition and reward";
+
   return (
     <>
+      <Head>
+        <title>{displayTitle}</title>
+      </Head>
       <Header />
       {errors && errors.length > 0 && (
         <Box as="section" backgroundColor="red" color="white" p={5} m={10}>
